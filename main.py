e4a0bca97735fa78ba9c1a8faa0a9d461555f51b
Ivan Loseev <loseev5@gmail.com>
2026-02-27 01:50:15 +0300

    ship

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from pyzkaccess import ZKAccess
 print('hello')
 
 found = ZKAccess.search_devices('192.168.1.255', dllpath='./plcommpro.dll')
-print("found {found.count} devices")
+print(f"found {found.count} devices")
