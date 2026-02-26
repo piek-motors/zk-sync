6150aae75dc2207cd657a77a834059f0806c4fd2
Ivan Loseev <loseev5@gmail.com>
2026-02-27 01:37:42 +0300

    f

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from pyzkaccess import ZKAccess
 
 print('hello')
 
-found = ZKAccess.search_devices('192.168.1.255')
+found = ZKAccess.search_devices('192.168.1.255', dllpath='./plcommpro.dll')
 print(found)
