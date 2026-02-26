9a08a972419b548794299d4f93b6d556875da4a5
Ivan Loseev <loseev5@gmail.com>
2026-02-27 02:08:42 +0300

    ignore

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ from pyzkaccess import ZKAccess
 
 import config
 
-# devices = ZKAccess.search_devices(dllpath='./plcommpro.dll')
-# print(f"found {len(devices)} devices", devices)
-print(f"config {config}")
 
-ZKAccess
+def fetch_all_transactions():
+    """Fetch all transactions from all configured devices."""
+    all_transactions = []
+    
+    for ip_code in config.config['ip_codes']:
+        ip = ip_code['ip']
+        code = ip_code['code']
+        
+        connstr = f'protocol=TCP,ipaddress={ip},port=4370,timeout=4000,passwd={config.config["password"]}'
+        
+        with ZKAccess(connstr=connstr) as zk:
+            transactions = zk.table('Transaction')
+            for txn in transactions:
+                txn_dict = txn.dict
+                txn_dict['device_code'] = code
+                txn_dict['device_ip'] = ip
+                all_transactions.append(txn_dict)
+    
+    return all_transactions
+
+
+if __name__ == '__main__':
+    transactions = fetch_all_transactions()
+    print(f"Fetched {len(transactions)} transactions")
+    for txn in transactions[:5]:  # Print first 5 as sample
+        print(txn)
